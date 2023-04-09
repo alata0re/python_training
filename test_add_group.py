9417fa33d1993298bc501e945f4ffe9ef5bce710
Yulia K <alataore@gmail.com>
2023-04-09 22:59:08 +0300

    Успешный тест

--- test_add_group.py
+++ test_add_group.py
@@ -1,52 +1,49 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest2
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import unittest
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
-class TestTraining(unittest2.TestCase):
+
+class TestTraining(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Chrome(executable_path=r'env\Lib\site-packages\selenium\webdriver\chrome')
         self.wd.implicitly_wait(60)
-
 
     def test_training(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new_group")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("poslednyaya nervnaya kletka")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("good bay")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.NAME, "pass").send_keys(Keys.ENTER)
+        WebDriverWait(self.wd, 5).until(ec.visibility_of_element_located((By.ID, "footer")))
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        WebDriverWait(self.wd, 5).until(ec.visibility_of_element_located((By.ID, "footer")))
+        wd.find_element(By.NAME, "new").click()
+        WebDriverWait(self.wd, 5).until(ec.visibility_of_element_located((By.ID, "footer")))
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys("new_group")
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys("poslednyaya nervnaya kletka")
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys("good bay")
+        wd.find_element(By.NAME, "submit").click()
+        WebDriverWait(self.wd, 5).until(ec.visibility_of_element_located((By.ID, "footer")))
+        wd.find_element(By.LINK_TEXT, "group page").click()
+        WebDriverWait(self.wd, 5).until(ec.visibility_of_element_located((By.ID, "footer")))
+        wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def tearDown(self):
         self.wd.quit()
 
+
 if __name__ == "__main__":
-    unittest2.main()
+    unittest.main()
